977c3b49706ce1238254334fb912ec99fef46cde
luochen01 <cluo8@uci.edu>
2018-03-10 17:14:14 -0800

    change tweet size to 0.5KB

--- storage-experiments/src/edu/uci/asterixdb/storage/experiments/feed/gen/DataGenerator.java
+++ storage-experiments/src/edu/uci/asterixdb/storage/experiments/feed/gen/DataGenerator.java
@@ -369,7 +369,7 @@ public class DataGenerator {
             buffer.position(0);
             buffer.limit(5000);
             referredTopics.clear();
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 4; i++) {
                 fillBuffer();
             }
             buffer.flip();
