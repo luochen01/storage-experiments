2658f47f91914c41220c5b030995f074780f659c
luochen <cluo8@uci.edu>
2020-09-23 19:36:38 -0700

    fix some queries

--- storage-experiments/src/Solution.java
+++ storage-experiments/src/Solution.java
@@ -1,67 +1,96 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Iterator;
 
-class Pair {
-    final int num;
-    final int index;
+class Node {
+    final int val;
 
-    public Pair(int num, int index) {
-        this.num = num;
-        this.index = index;
+    Node left;
+    Node right;
+    Node parent;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+        left.parent = this;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+        right.parent = this;
+    }
+
+}
+
+class TreeIterator implements Iterator<Integer> {
+
+    private Node node;
+    private Integer next = null;
+
+    public TreeIterator(Node node) {
+        this.node = getFirstChild(node);
+    }
+
+    private Node getFirstChild(Node node) {
+        while (node.left != null || node.right != null) {
+            if (node.left != null) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node;
     }
 
     @Override
-    public String toString() {
-        return num + "@" + index;
+    public boolean hasNext() {
+        if (next == null) {
+            doGetNext();
+        }
+        return next != null;
     }
+
+    @Override
+    public Integer next() {
+        if (next == null) {
+            doGetNext();
+        }
+        return next;
+    }
+
+    private void doGetNext() {
+        if (node != null) {
+            next = node.val;
+            Node parent = node.parent;
+            if (parent != null) {
+                if (parent.left == node && parent.right != null) {
+                    node = parent.right;
+                    while (node.left != null || node.right != null) {
+                        if (node.left != null) {
+                            node = node.left;
+                        } else {
+                            node = getFirstChild(node.right);
+                        }
+                    }
+                } else {
+                    node = parent;
+                }
+            } else {
+                node = null;
+            }
+        }
+    }
+
 }
 
 class Solution {
 
-    public int longestSubarray(int[] nums, int k) {
-        //        TreeSet<Pair> set = new TreeSet<>((p1, p2) -> {
-        //            int cmp = Integer.compare(p1.num, p2.num);
-        //            if (cmp != 0) {
-        //                return cmp;
-        //            } else {
-        //                return Integer.compare(p1.index, p2.index);
-        //            }
-        //        });
-        List<Pair> list = new ArrayList<>();
-        int max = 1;
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (sum <= k) {
-                max = Math.max(max, i + 1);
-            }
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (sum < min) {
-                list.add(new Pair(sum, i));
-                min = sum;
-            }
-            sum -= nums[i];
-        }
-        Collections.reverse(list);
-        int right = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            while (right < list.size() && list.get(right).num - sum <= k) {
-                maxIndex = list.get(right).index;
-                right++;
-            }
-            max = Math.max(max, maxIndex - i);
-        }
-
-        return max;
-    }
-
     public static void main(String[] args) {
-        int[] array = new int[] { 2000, 10, 2000, 10, -100, 200, -1000 };
-        System.out.println(new Solution().longestSubarray(array, 20));
+        Node root = new Node(1);
+        Node left = new Node(2);
+        root.setLeft(left);
+        Node leftLeft = new Node(3);
+
     }
 }
